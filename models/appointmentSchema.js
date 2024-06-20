c94ac3236d782fa1f1c3391766684f1ab9722749
Druhan <druhan@duck.com>
2024-06-21 00:52:18 +0530

    done all functions to connect to frontend

--- models/appointmentSchema.js
+++ models/appointmentSchema.js
@@ -58,7 +58,7 @@ const appointmentSchema=new mongoose.Schema({
     },
     hasVisited:{
         type:Boolean,
-        required:true
+        default:false 
     },
     doctorId:{
         type:mongoose.Schema.ObjectId,
