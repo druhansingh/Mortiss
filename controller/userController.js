60d5e25fce0df2ec20a024a83e21cd2df4567748
Druhan <druhan@duck.com>
2024-06-20 17:16:04 +0530

    user is able to log in successfully

--- controller/userController.js
+++ controller/userController.js
@@ -17,3 +17,28 @@ export const patientRegister=catchAsyncErrors(async(req,res,next)=>{
         message:"User Registered"
     })
 })
+
+export const login=catchAsyncErrors(async(req,res,next)=>{
+    const{email,password,confirmPassword,role}=req.body;
+    if(!email||!password||!confirmPassword||!role){
+return next (new ErrorHandler("Please provide all details! ",400))
+    }
+    if (password!==confirmPassword){
+        return next (new ErrorHandler("Passwords do not match! ",400))
+    }
+    const user=await User.findOne({email}).select("+password");
+    if(!user){
+        return next (new ErrorHandler("Ivalid email or password! ",400))
+    }
+    const isPasswordMatched=await user.comparePassword(password);
+    if(!isPasswordMatched){
+        return next (new ErrorHandler("Ivalid email or password! ",400))
+    }
+    if(role!==user.role){
+        return next (new ErrorHandler("User with this role not found! ",400))
+    }
+    res.status(200).json({
+        success:true,
+        message:"User Logged in successfully! "
+    })
+})
