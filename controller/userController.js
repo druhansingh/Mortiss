ce0c1e44e63d3e259030057ab3b5aec843d7b943
Druhan <druhan@duck.com>
2024-06-20 16:49:04 +0530

    Finally made a patient register, dob automatic on froontend

--- controller/userController.js
+++ controller/userController.js
@@ -1,11 +1,11 @@
-import {catchAsyncErrors} from "../middlewares/catchAsyncErrors.js"
-import {ErrorHandler} from "../middlewares/errorMiddleware.js"
-import {User} from "../models/userSchema.js"
+import {catchAsyncErrors} from "../middlewares/catchAsyncErrors.js";
+import ErrorHandler from "../middlewares/errorMiddleware.js";
+import {User} from "../models/userSchema.js";
 
 export const patientRegister=catchAsyncErrors(async(req,res,next)=>{
     const {firstName,lastName,email,phone,password,gender,dob,nic,role}=req.body;
     if(!firstName||!lastName||!email||!phone||!password||!gender||!dob||!nic||!role){
-        return next(new ErrorHandler("Please fill full form! ",400))
+        return next(new ErrorHandler("Please fill full form! ",400));
     }
     let user=await User.findOne({email})
     if(user){
