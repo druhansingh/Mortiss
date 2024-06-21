01211d839b08d19bb875f4733e48926e98c80507
Druhan <druhan@duck.com>
2024-06-22 04:50:24 +0530

    made user login and logout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,11 +9,28 @@ import Login from "./pages/Login"
 import { ToastContainer} from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
+import Navbar from "./components/Navbar";
+import { Context } from "./main";
 
 const App = () => {
+  const{isAuthenticated,setIsAuthenticated,setUser}=useContext(Context);
+  useEffect(()=>{
+    const fetchUser=async()=>{
+      try {
+        const response=await axios.get("http://localhost:4000/api/v1/user/patient/me",{withCredentials:true})
+        setIsAuthenticated(true)
+        setUser(response.data.user)
+      } catch (error) {
+        setIsAuthenticated(false)
+        setUser({})
+      }
+    }
+    fetchUser()
+  },[isAuthenticated])
   return (
     <>
     <Router>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/appointment' element={<Appointment/>}/>
