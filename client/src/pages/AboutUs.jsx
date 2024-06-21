878acf1bf4d459b3b8e678dbc4d9550a7928a145
Druhan <druhan@duck.com>
2024-06-21 05:52:10 +0530

    made initial frontend and basic components

---
+++ client/src/pages/AboutUs.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const aboutUs = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default aboutUs
---
+++ client/src/pages/Appointment.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const appointment = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default appointment
